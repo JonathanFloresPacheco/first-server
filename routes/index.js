130c892d46ece61b938630a81474a361ef838022
Jonathan Flores Pacheco <33069854+JonathanFloresPacheco@users.noreply.github.com>
2021-06-15 11:14:46 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 const exprees = require('express');
 const router = exprees.Router();
 const IndexController = require('../controllers/index')
-const items = [
-    {id:1,name:"product1" },
-    {id:2,name:"product2" },
-    {id:3,name:"product3" },
-];
 
 router.get('/',IndexController.index);
 
